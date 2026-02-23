b83b9ebc5e311f6bad939a9b90c36dca2d7087c6
MGarciaRdz <mgarcia220904@hotmail.com>
2026-02-23 13:05:31 -0600

    correcion de problema del metodo newton-raphson

--- backend/metodos/newton_raphson.py
+++ backend/metodos/newton_raphson.py
@@ -1,35 +1,15 @@
-import sympy as sp
-
-
 def newton_raphson(func, x0, tol=1e-7, max_iter=1000):
-
-    if not callable(func):
-        print("Error: 'func' debe ser una función.")
-        return None
-
-    x = sp.symbols('x')
-    f_sym = func(x)
-    f_prime_sym = sp.diff(f_sym, x)
-
-    f_num = sp.lambdify(x, f_sym, 'math')
-    f_prime_num = sp.lambdify(x, f_prime_sym, 'math')
-
     x_n = x0
     historial = [x_n]
 
     for i in range(max_iter):
+        f_x_n = func(x_n)
+        # derivada numérica
+        h = 1e-6
+        f_prime_x_n = (func(x_n + h) - func(x_n - h)) / (2*h)
 
-        try:
-            f_x_n = f_num(x_n)
-            f_prime_x_n = f_prime_num(x_n)
-        except Exception as e:
-            print("Error evaluando la función:", e)
-            return None
-
-        # división entre cero (numérica)
         if abs(f_prime_x_n) < 1e-12:
-            print("Error: derivada cercana a cero.")
-            return None
+            raise ValueError("Derivada cercana a cero")
 
         x_n1 = x_n - f_x_n / f_prime_x_n
         historial.append(x_n1)
@@ -39,5 +19,4 @@ def newton_raphson(func, x0, tol=1e-7, max_iter=1000):
 
         x_n = x_n1
 
-    print("No convergió.")
-    return None, historial
+    raise ValueError("No convergió")
